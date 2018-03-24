678c731d36406b01aacdc0ba1a01aaaeeca32cf7
sunilkumar <rsunilkumar.2186@gmail.com>
2018-03-24 14:04:57 +0530

    All centro files -first commit

---
+++ src/main/java/Sucess.java
@@ -0,0 +1,6 @@
+
+public class Sucess {
+ public String Centro() {
+	 return "sample program";
+ }
+}
---
+++ src/test/java/SucessTest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SucessTest {
+
+	@Test
+	public void test() {
+		
+	}
+
+}
